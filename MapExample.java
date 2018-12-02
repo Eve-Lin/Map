47de6b67933828789b95aa5656352581909c68d0
Eve Lin <evelin.dicheva@gmail.com>
2018-12-02 19:42:59 +0200

    Map Example using Lambda Expressions

---
+++ MapExample.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+
+	public static void main(String args[]){
+		Map <String, String> myMap = new HashMap<>();
+		
+		myMap.put("2222","ghi");
+		myMap.put("3333", "abc");
+		myMap.put("1111","def");
+		
+		myMap.forEach((k,v) -> System.out.println(k + "|" + v));
+	}
+	
+	
+	
+	
+}
